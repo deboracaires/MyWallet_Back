dd6081d2e674390d09add424c53654db389c07cf
deboracaires <deboracaires@pop-os.localdomain>
2021-11-26 12:33:38 -0300

    all tests for route post /sign-in passing

--- tests/integration/signUp.test.js
+++ tests/integration/signUp.test.js
@@ -8,6 +8,8 @@ import { clearDatabase, closeConnection } from '../utils/database.js';
 
 import app from '../../src/app.js';
 
+const invalidBody = {};
+
 beforeEach(async () => {
   await clearDatabase();
 });
@@ -26,8 +28,6 @@ describe('POST /sign-up', () => {
     };
   }
   it('should answer with status 400 when body is invalid', async () => {
-    const invalidBody = {};
-
     const response = await supertest(app).post('/sign-up').send(invalidBody);
     expect(response.status).toEqual(400);
   });
@@ -46,5 +46,41 @@ describe('POST /sign-up', () => {
 
     const response = await supertest(app).post('/sign-up').send(body);
     expect(response.status).toEqual(201);
+  });
+});
+
+describe('POST /sign-in', () => {
+  function generateBody({ name, email, password } = {}) {
+    return {
+      name: name || faker.name.findName(),
+      email: email || faker.internet.email(),
+      password: password || '123456',
+    };
+  }
+  it('should answer with status 400 when body is invalid', async () => {
+    const response = await supertest(app).post('/sign-in').send(invalidBody);
+    expect(response.status).toEqual(400);
+  });
+  it('should answer with status 401 when email is not registered', async () => {
+    const body = generateBody();
+
+    const response = await supertest(app).post('/sign-in').send(body);
+    expect(response.status).toEqual(401);
+  });
+  it('should answer with status 401 when email registered but the user gives an wrong password', async () => {
+    const body = generateBody();
+
+    await createUser({ email: body.email, password: '12' });
+
+    const response = await supertest(app).post('/sign-in').send(body);
+    expect(response.status).toEqual(401);
+  });
+  it('should answer with status 200 when email exists and password is correct', async () => {
+    const body = generateBody();
+
+    await createUser(body);
+
+    const response = await supertest(app).post('/sign-in').send(body);
+    expect(response.status).toEqual(200);
   });
 });
