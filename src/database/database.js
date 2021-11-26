631aa8a4567caaaba77639994a02568d0b36a304
deboracaires <deboracaires@pop-os.localdomain>
2021-11-26 14:24:36 -0300

    database to run tests locally

--- src/database/database.js
+++ src/database/database.js
@@ -1,12 +1,25 @@
+/* eslint-disable no-tabs */
+/* eslint-disable no-mixed-spaces-and-tabs */
 import pg from 'pg';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const { Pool } = pg;
 
-const connection = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+const config =	process.env.NODE_ENV === 'test'
+	  ? {
+	    user: process.env.DB_USER,
+	    password: process.env.DB_PASSWORD,
+	    host: process.env.DB_HOST,
+	    port: process.env.DB_PORT,
+	    database: process.env.DB_DATABASE,
+		  }
+	  : {
+	    connectionString: process.env.DATABASE_URL,
+	    ssl: {
+	      rejectUnauthorized: false,
+	    },
+		  };
+const connection = new Pool(config);
 
 export default connection;
